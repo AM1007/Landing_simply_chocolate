6ff5eea94b28ed3c557f9d94f3cdc3cf614add47
Andrew Motko <andrew.motko@gmail.com>
2023-10-23 03:10:40 +0300

    products section swiper added

---
+++ src/js/swiper.js
@@ -0,0 +1,28 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+new Swiper('.swiper', {
+  modules: [Navigation],
+
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 19,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 19,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
