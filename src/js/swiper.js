5969bea56c516a6ffad8150778f05cd382d7d402
Andrew Motko <andrew.motko@gmail.com>
2023-10-23 03:21:27 +0300

    swiper bug fixed

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,5 +1,7 @@
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 new Swiper('.swiper', {
   modules: [Navigation],
